f22f7e200a55b384e95696d1998a37b9028437c9
Jensen Ruud <jensen.is.testing@gmail.com>
2022-03-31 13:25:26 -0500

    Tested all functions of task and toDoList class

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const task = (newTitle, newDesc, newDue, newMatrix) => {
 
   const getDesc = () => desc;
 
-  const getDueDate = () => due.getTime();
+  const getDueDate = () => due;
 
   const getMatrix = () => matrix;
 
@@ -57,8 +57,8 @@ const toDoList = () => {
   const sortByDate = () => {
     for (let i = 0; i < list.length - 1; i++) {
       for (let j = i; j < list.length - 1; j++) {
-        if (list[j].getDueDate > list[j + 1].getDueDate)
-          swapElements(list[j], list[j + 1]);
+        if (list[j].getDueDate() > list[j + 1].getDueDate())
+          swapElements(j, j + 1);
       }
     }
   };
@@ -73,8 +73,8 @@ const toDoList = () => {
     let doneItems = [];
     let notDoneItems = [];
 
-    notDoneItems = filter((item) => item.isDone() == false);
-    doneItems = filter((item) => item.isDone() == true);
+    notDoneItems = list.filter((item) => item.isDone() == false);
+    doneItems = list.filter((item) => item.isDone() == true);
 
     list = notDoneItems.concat(doneItems);
   };
@@ -82,7 +82,7 @@ const toDoList = () => {
   const getMatrix = (matrixType) => {
     let filteredList = [];
 
-    filteredList = filter((item) => item.getMatrix() == matrixType);
+    filteredList = list.filter((item) => item.getMatrix() == matrixType);
 
     return filteredList;
   };
@@ -95,5 +95,124 @@ const toDoList = () => {
     return filteredList.length;
   };
 
-  return { getList, getItem, addTask, delTask, countMatrix };
+  return { getList, getItem, addTask, delTask, getMatrix, countMatrix };
 };
+
+//Beginning of DOM mainpulation
+
+function nukeList() {
+  document.querySelector(".list_content").innerHTML = "";
+}
+
+function createTaskCard(currentTask, pos) {
+  let taskCard = document.createDocumentFragment();
+  let element = document.createElement("div");
+
+  element.className = "task_card";
+  taskCard.appendChild(element);
+
+  element = divWithClass("checkbox");
+  element.setAttribute("data-pos", pos);
+  element.innerHTML = "C";
+  taskCard.querySelector(".task_card").appendChild(element);
+
+  element = divWithClass("task_info");
+  taskCard.querySelector(".task_card").appendChild(element);
+
+  element = divWithClass("info_divide");
+  taskCard.querySelector(".task_info").appendChild(element);
+
+  element = divWithClass("matrix_src");
+  element.innerHTML = currentTask.getMatrix();
+  taskCard.querySelector(".info_divide").appendChild(element);
+
+  element = divWithClass("due_date");
+  element.innerHTML = currentTask.getDueDate();
+  taskCard.querySelector(".info_divide").appendChild(element);
+
+  element = divWithClass("task_title");
+  element.innerHTML = currentTask.getTitle();
+  taskCard.querySelector(".info_divide").appendChild(element);
+
+  element = divWithClass("task_desc");
+  element.innerHTML = currentTask.getDesc();
+  taskCard.querySelector(".task_info").appendChild(element);
+
+  element = divWithClass("options");
+  taskCard.querySelector(".task_card").appendChild(element);
+
+  element = divWithClass("edit");
+  element.setAttribute("data-pos", pos);
+  element.innerHTML = "Edit";
+  taskCard.querySelector(".options").appendChild(element);
+
+  element = divWithClass("delete");
+  element.setAttribute("data-pos", pos);
+  element.innerHTML = "Delete";
+  taskCard.querySelector(".options").appendChild(element);
+
+  document.querySelector(".list_content").appendChild(taskCard);
+}
+
+function divWithClass(newClass) {
+  let element = document.createElement("div");
+  element.className = newClass;
+  return element;
+}
+
+function renderList(currentList) {
+  for (let i = 0; i < currentList.length; i++) {
+    createTaskCard(currentList[i], i);
+  }
+}
+
+var trialTask1 = task(
+  "Make Grocery List",
+  "Make a grocery list before you pick up Jennita tonight",
+  100000000,
+  "UI"
+);
+
+var trialTask2 = task(
+  "Pick up Jennita",
+  "Get Jennita from the airport",
+  100100000,
+  "UI"
+);
+
+var trialTask3 = task(
+  "Create a to-do list website",
+  "Make a to-do list website to fulfill the project requirements",
+  90000000,
+  "NI"
+);
+
+var trialTask4 = task(
+  "Sweep kitty litter",
+  "Sweep up the kitty litter in the laundry room",
+  90200000,
+  "UN"
+);
+
+var trialTask5 = task(
+  "Watch YouTube videos",
+  "Waste time and slack off by watching videos online",
+  90300000,
+  "NN"
+);
+
+trialTask5.toggleDone();
+
+var currentList = toDoList();
+
+currentList.addTask(trialTask1);
+currentList.addTask(trialTask5);
+currentList.addTask(trialTask3);
+currentList.addTask(trialTask4);
+currentList.addTask(trialTask2);
+
+console.log(currentList.countMatrix("UI"));
+
+renderList(currentList.getMatrix("UI"));
+
+console.log(currentList.getList());
